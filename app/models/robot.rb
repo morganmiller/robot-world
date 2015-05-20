be2987dcf30a0f63eebf391824702b23cfc3b27c
Morgan Miller <m.miller722@gmail.com>
2015-05-20 16:15:11 -0600

    added signs

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -13,5 +13,36 @@ class Robot
     @favorite_color = data["favorite_color"]
     @avatar         = "https://robohash.org/#{@name}.png"
   end
+  
+  def sign
+    date = Date.parse(@birth_date).strftime("%m%d").to_i
+    if (321..419).to_a.include?(date)
+      "Aries"
+    elsif (420..520).to_a.include?(date)
+      "Taurus"
+    elsif (521..620).to_a.include?(date)
+      "Gemini"
+    elsif (621..722).to_a.include?(date)
+      "Cancer"
+    elsif (723..822).to_a.include?(date)
+      "Leo"
+    elsif (823..922).to_a.include?(date)
+      "Virgo"
+    elsif (923..1022).to_a.include?(date)
+      "Libra"
+    elsif (1023..1121).to_a.include?(date)
+      "Scorpio"
+    elsif (1122..1221).to_a.include?(date)
+      "Sagittarius"
+    elsif ((1222..1231).to_a + (101..119).to_a).include?(date)
+      "Capricorn"
+    elsif (120..218).to_a.include?(date)
+      "Aquarius"
+    elsif (219..320).to_a.include?(date)
+      "Pisces"
+    else
+      "Invalid"
+    end
+  end
 
 end
